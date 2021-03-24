62183ae4bf219ffefec59e200cf659b7eba2d28c
duylebkHCM <duy.le.bku_16039@hcmut.edu.vn>
2021-03-24 19:10:29 +0700

    Modified endpoint

--- shares/api/src/main/java/com/marvin/shares/api/account/AccountService.java
+++ shares/api/src/main/java/com/marvin/shares/api/account/AccountService.java
@@ -2,7 +2,9 @@ package com.marvin.shares.api.account;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/api/v1/")
 public interface AccountService {
 
     /**
@@ -15,6 +17,5 @@ public interface AccountService {
             value    = "/account/{accountId}",
             produces = "application/json")
     AccountDto getAccount(@PathVariable int accountId);
-
 }
 
